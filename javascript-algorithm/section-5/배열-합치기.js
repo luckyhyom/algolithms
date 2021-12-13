3929dfcb6c7c16bacba2e25318444444b5388e6f
luckyhyom <bs_khm@naver.com>
2021-12-13 15:54:45 +0900

    배열 합치기 (투포인터, sort는 n log n이므로 반복문 두번이 더 나음)

---
+++ javascript-algorithm/section-5/배열-합치기.js
@@ -0,0 +1,22 @@
+let input = [1,3,5];
+let input2 = [2,3,6,7,9];
+
+function concats(arr,arr2) {
+    return arr.concat(arr2).sort((a,b)=>a-b);
+}
+function concats2(arr,arr2) {
+    let result = [];
+    let n = arr.length;
+    let m = arr2.length;
+    let p1, p2 = 0;
+
+    while (p1 < n && p2 < m) {
+        if (arr[p1] < arr2[p2]) result.push(arr[p1++]);
+        else result.push(arr2[p2++]);
+    }
+    while (p1 < n) result.push(arr[p1++]);
+    while (p2 < m) result.push(arr2[p2++]);
+    return result;
+}
+
+console.log(concats(input,input2));
