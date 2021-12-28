0f3b43948c111ead4f59bc0523fbbac36330ff9e
luckyhyom <bs_khm@naver.com>
2021-12-28 15:04:25 +0900

    X 연속 부분 수열2
    - 말 그대로 연속이기때문에 인덱스를 이용해 길이를 구한다. (순서를 뒤바꾸거나 하지 않는다)
    - while을 이용해서 조건에 맞을 때 까지 세탁(?) 한다.

---
+++ javascript-algorithm/section-5/연속-부분-수열2.js
@@ -0,0 +1,19 @@
+let input = [1,2,1,2,3];
+// let input = [1,3,5,2,3];
+// 1,3일때 카운트가 3개가 되어야함
+let expected = 5
+let result = 10;
+
+function mySolution(arr, expected) {
+    let lt=0, sum=0, count = 0;
+    for (let rt = 0; rt < arr.length; rt++) {
+        sum+=arr[rt];
+        while (sum > expected) {
+            sum -= arr[lt++]
+        }
+        count += (lt-rt+1);
+    }
+    return result;
+}
+
+console.log(mySolution(input, expected));
