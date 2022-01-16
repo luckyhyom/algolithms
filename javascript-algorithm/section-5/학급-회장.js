9be085ab63d8729c8c3011ed63163bff821f537d
luckyhyom <bs_khm@naver.com>
2022-01-16 20:46:56 +0900

    학급회장 해답

--- javascript-algorithm/section-5/학급-회장.js
+++ javascript-algorithm/section-5/학급-회장.js
@@ -23,3 +23,23 @@ function mySolution(votes) {
 
 const input = ['B', 'A', 'C', 'B', 'A', 'C', 'C', 'A', 'C', 'C', 'B', 'D', 'E', 'D', 'E'];
 console.log(mySolution(input));
+
+function solution(s){  
+    let answer;
+    let sH = new Map();
+    for(let x of s){
+        if(sH.has(x)) sH.set(x, sH.get(x)+1);
+        else sH.set(x, 1);
+    }
+    let max=Number.MIN_SAFE_INTEGER;
+    for(let [key, val] of sH){
+        if(val>max){
+            max=val;
+            answer=key;
+        }
+    }
+    return answer;
+}
+
+let str="BACBACCACCBDEDE";
+console.log(solution(str));
