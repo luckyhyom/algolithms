e6c528dd4668f7cb7dc4230cbc998cd6879ecfc7
luckyhyom <bs_khm@naver.com>
2022-01-20 00:06:53 +0900

    괄호 문자 제거 (스택)
    - ')'가 나오면, '('가 나올때까지 pop하기
      - pop의 결과가 '('임을 알았을때는 '('이 제거 된 후임. 제거하면서 값을 반환하기 때문

--- javascript-algorithm/section-6/괄호-문자-제거.js
+++ javascript-algorithm/section-6/괄호-문자-제거.js
@@ -10,4 +10,17 @@ function mySolution(parentheses) {
     return answer;
 }
 
+function solution(s){  
+    let answer;
+    let stack=[];
+    for(let x of s){
+        if(x===')'){
+            while(stack.pop()!=='(');
+        }
+        else stack.push(x);
+    }
+    answer=stack.join('');
+    return answer;
+}
+
 console.log(mySolution('(A(BC)D)EF(G(H)(IJ)K)LM(N)'));
