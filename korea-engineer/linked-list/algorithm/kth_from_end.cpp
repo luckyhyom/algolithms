b173efbf1cb97e7f2bdb1344d079a81b35f2b657
luckyhyom <bs_khm@naver.com>
2025-11-11 19:18:35 +0900

    algorithm: kth from end using recursion

---
+++ korea-engineer/linked-list/algorithm/kth_from_end.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+
+class LinkedList {
+    class Node {
+       public:
+        int data;
+        Node* next = nullptr;
+        Node(int n) : data(n) {}
+    };
+
+    Node* header;
+
+   public:
+    LinkedList() { header = new Node(0); }
+
+    void append(int n) {
+        Node* last = header;
+        while (last->next != nullptr) {
+            last = last->next;
+        }
+        last->next = new Node(n);
+    }
+
+    void remove(int n) {
+        Node* last = header;
+        while (last->next != nullptr) {
+            if (last->next->data == n) {
+                Node* found = last->next;
+                last->next = last->next->next;
+                delete found;
+            } else {
+                last = last->next;
+            }
+        }
+    }
+
+    void retrieve() {
+        Node* n = header;
+        while (n->next != nullptr) {
+            std::cout << n->next->data << std::endl;
+            n = n->next;
+        }
+    }
+
+    void removeDups() {
+        Node* n = header;
+
+        while (n != nullptr && n->next != nullptr) {
+            Node* r = n;
+            while (r->next != nullptr) {
+                if (n->data == r->next->data) {
+                    Node* found = r->next;
+                    r->next = r->next->next;
+                    delete found;
+                } else {
+                    r = r->next;
+                }
+            }
+            n = n->next;
+        }
+    }
+
+    /**
+     * 방법 1.
+     * 1. 길이 측정
+     * 2. 길이 - k + 1
+     * 3. (2)만큼 이동하여 값 추출
+     *
+     * 방법 2.
+     * 재귀 호출 사용
+     */
+
+    void printKthFromEnd(int n) {
+        if (n <= 0) {
+            std::cout << "n shoud be lager than 0" << std::endl;
+            return;
+        }
+
+        int count = 0;
+        Node* result = kthFromEnd(header->next, n, &count);
+        if (result == nullptr) {
+            std::cout << "No Result" << std::endl;
+        }
+    }
+
+   private:
+    Node* kthFromEnd(Node* node, int n, int* count) {
+        Node* result;
+        if (node->next != nullptr) {
+            result = kthFromEnd(node->next, n, count);
+
+            if (result != nullptr) {
+                return result;
+            }
+        }
+
+        ++(*count);
+        if (n == *count) {
+            std::cout << node->data << std::endl;
+            result = node;
+        } else {
+            result = nullptr;
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    LinkedList ll;
+    ll.append(1);
+    ll.append(2);
+    ll.append(3);
+    ll.append(4);
+
+    ll.printKthFromEnd(1);  // 4
+    ll.printKthFromEnd(2);  // 3
+    ll.printKthFromEnd(3);  // 2
+    ll.printKthFromEnd(4);  // 1
+    ll.printKthFromEnd(5);  // No Result
+    ll.printKthFromEnd(0);  // n shoud be lager than 0
+    return 0;
+}
