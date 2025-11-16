f245a4af7fa43fa52d3dbc37752b9f93b1b0868e
luckyhyom <bs_khm@naver.com>
2025-11-16 21:21:11 +0900

    altorithm: Sum v4 (LinkedList)

--- korea-engineer/linked-list/algorithm/sum.cpp
+++ korea-engineer/linked-list/algorithm/sum.cpp
@@ -129,6 +129,83 @@ LinkedList* sum_v3(LinkedList::Node* n1, LinkedList::Node* n2, int c) {
 
     return ll;
 }
+
+int getLength(LinkedList::Node* node) {
+    if (node == nullptr) {
+        return 0;
+    }
+    int result = 1;
+    while (node->next != nullptr) {
+        ++result;
+        node = node->next;
+    }
+    return result;
+}
+
+LinkedList::Node* insertBefore(LinkedList::Node* node, int value) {
+    if (node == nullptr) {
+        return node;
+    }
+
+    LinkedList::Node* result = new LinkedList::Node(value, 0);
+    result->append(node);
+    return result;
+}
+
+LinkedList::Node* LPadList(LinkedList::Node* node, int length) {
+    for (size_t i = 0; i < length; i++) {
+        node = insertBefore(node, 0);
+    }
+    return node;
+}
+
+/**
+ * 1. 각 리스트의 길이를 구한다.
+ * 2. 길이가 짧은 리스트의 길이를 동일하게 설정한다.
+ * 3. 맨 마지막 노드부터 더한다.
+ * 4. carry를 저장하기위한 포인터 변수 필요하다.
+ * 5. result는 왜필요하지?
+ */
+LinkedList* sum_v4_a(LinkedList::Node* n1, LinkedList::Node* n2, int* c) {
+    if (n1 == nullptr) {
+        return nullptr;
+    }
+
+    // 419 + 034
+
+    LinkedList* result = new LinkedList();
+    LinkedList* next = nullptr;
+    if (n1->next != nullptr) {
+        next = sum_v4_a(n1->next, n2->next, c);
+    }
+
+    int sum = n1->data + n2->data + *c;
+    *c = sum >= 10 ? 1 : 0;
+    int value = sum % 10;
+    result->append(value);
+
+    if (next != nullptr) {
+        result->append(next->get(1));
+    }
+
+    return result;
+}
+
+LinkedList* sum_v4(LinkedList::Node* n1, LinkedList::Node* n2) {
+    // 재귀는 언제 돌리지, 얘는 반복실행될 필요 없을 것 같은데.
+    int n1L = getLength(n1);
+    int n2L = getLength(n2);
+
+    if (n1L > n2L) {
+        n2 = LPadList(n2, n1L - n2L);
+    } else if (n2L > n1L) {
+        n1 = LPadList(n1, n2L - n1L);
+    }
+    int c = 0;
+    LinkedList* result = sum_v4_a(n1, n2, &c);
+    return result;
+}
+
 /**
  * LL에는 값이 1의 자리부터 순차적으로 들어있다.
  * 1200이면 0021
@@ -158,20 +235,32 @@ int main() {
     LinkedList* result2 = sum_v2(&ll, &ll2);
     result2->retrieve();
 
-    LinkedList ll3;
-    ll3.append(5);
-    ll3.append(6);
-    ll3.append(7);
-    ll3.append(2);
+    ll.reset();
+    ll2.reset();
 
-    LinkedList ll4;
-    ll4.append(6);
-    ll4.append(3);
-    ll4.append(2);
-    ll4.append(1);
-    ll4.append(5);
+    ll.append(5);
+    ll.append(6);
+    ll.append(7);
+    ll.append(2);
 
-    LinkedList* sum3 = sum_v3(ll3.get(1), ll4.get(1), 0);  // 54001
-    std::cout << "FUCK" << std::endl;
+    ll2.append(6);
+    ll2.append(3);
+    ll2.append(2);
+    ll2.append(1);
+    ll2.append(5);
+
+    LinkedList* sum3 = sum_v3(ll.get(1), ll2.get(1), 0);  // 54001
     sum3->retrieve();
+
+    ll.reset();
+    ll2.reset();
+
+    ll.append(4);
+    ll.append(1);
+    ll.append(9);
+
+    ll2.append(3);
+    ll2.append(4);
+    LinkedList* sum4 = sum_v4(ll.get(1), ll2.get(1));  // 54001
+    sum4->retrieve();
 }
