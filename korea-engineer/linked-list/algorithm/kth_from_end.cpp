ee76e3044dde6d82260393b11ab526a2e269d753
luckyhyom <bs_khm@naver.com>
2025-11-21 15:32:51 +0900

    algorithm: 링크드리스트 뒤에서 n번째 노드 출력하기

--- korea-engineer/linked-list/algorithm/kth_from_end.cpp
+++ korea-engineer/linked-list/algorithm/kth_from_end.cpp
@@ -1,110 +1,15 @@
-#include <iostream>
+#include "../linked-list.cpp"
 
-class LinkedList {
-    class Node {
-       public:
-        int data;
-        Node* next = nullptr;
-        Node(int n) : data(n) {}
-    };
-
-    Node* header;
-
-   public:
-    LinkedList() { header = new Node(0); }
-
-    void append(int n) {
-        Node* last = header;
-        while (last->next != nullptr) {
-            last = last->next;
-        }
-        last->next = new Node(n);
+void printKthFromEnd(LinkedList::Node* node, int k, int* count) {
+    if (node->next != nullptr) {
+        printKthFromEnd(node->next, k, count);
     }
 
-    void remove(int n) {
-        Node* last = header;
-        while (last->next != nullptr) {
-            if (last->next->data == n) {
-                Node* found = last->next;
-                last->next = last->next->next;
-                delete found;
-            } else {
-                last = last->next;
-            }
-        }
+    ++(*count);
+    if (*count == k) {
+        std::cout << node->data << std::endl;
     }
-
-    void retrieve() {
-        Node* n = header;
-        while (n->next != nullptr) {
-            std::cout << n->next->data << std::endl;
-            n = n->next;
-        }
-    }
-
-    void removeDups() {
-        Node* n = header;
-
-        while (n != nullptr && n->next != nullptr) {
-            Node* r = n;
-            while (r->next != nullptr) {
-                if (n->data == r->next->data) {
-                    Node* found = r->next;
-                    r->next = r->next->next;
-                    delete found;
-                } else {
-                    r = r->next;
-                }
-            }
-            n = n->next;
-        }
-    }
-
-    /**
-     * 방법 1.
-     * 1. 길이 측정
-     * 2. 길이 - k + 1
-     * 3. (2)만큼 이동하여 값 추출
-     *
-     * 방법 2.
-     * 재귀 호출 사용
-     */
-
-    void printKthFromEnd(int n) {
-        if (n <= 0) {
-            std::cout << "n shoud be lager than 0" << std::endl;
-            return;
-        }
-
-        int count = 0;
-        Node* result = kthFromEnd(header->next, n, &count);
-        if (result == nullptr) {
-            std::cout << "No Result" << std::endl;
-        }
-    }
-
-   private:
-    Node* kthFromEnd(Node* node, int n, int* count) {
-        Node* result;
-        if (node->next != nullptr) {
-            result = kthFromEnd(node->next, n, count);
-
-            if (result != nullptr) {
-                return result;
-            }
-        }
-
-        ++(*count);
-        if (n == *count) {
-            std::cout << node->data << std::endl;
-            result = node;
-        } else {
-            result = nullptr;
-        }
-
-        return result;
-    }
-};
+}
 
 int main() {
     LinkedList ll;
@@ -113,11 +18,13 @@ int main() {
     ll.append(3);
     ll.append(4);
 
-    ll.printKthFromEnd(1);  // 4
-    ll.printKthFromEnd(2);  // 3
-    ll.printKthFromEnd(3);  // 2
-    ll.printKthFromEnd(4);  // 1
-    ll.printKthFromEnd(5);  // No Result
-    ll.printKthFromEnd(0);  // n shoud be lager than 0
+    int count = 0;
+    printKthFromEnd(ll.get(1), 1, &count);  // 4
+    count = 0;
+    printKthFromEnd(ll.get(1), 2, &count);  // 3
+    count = 0;
+    printKthFromEnd(ll.get(1), 3, &count);  // 2
+    count = 0;
+    printKthFromEnd(ll.get(1), 4, &count);  // 1
     return 0;
 }
