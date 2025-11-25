22c1e9136c6adfc3a9e5a7740661030b4d054a8b
luckyhyom <bs_khm@naver.com>
2025-11-25 19:03:14 +0900

    ds: Queue

---
+++ korea-engineer/queue/ground/queue-251125.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+class Queue {
+    class Node {
+       public:
+        int data;
+        Node* next;
+
+        Node(int v) : data(v) {}
+    };
+
+    Node* head = nullptr;
+    Node* tail = nullptr;
+
+   public:
+    Queue() {}
+
+    void add(int v) {
+        Node* new_node = new Node(v);
+        if (head == nullptr) {
+            this->head = new_node;
+        } else {
+            this->tail->next = new_node;
+        }
+        this->tail = new_node;
+    }
+
+    int remove() {
+        if (this->head == nullptr) {
+            return 0;
+        }
+
+        int result = this->head->data;
+        Node* temp = this->head;
+
+        this->head = this->head->next;
+        delete temp;
+
+        if (this->head == nullptr) {
+            this->tail = nullptr;  // add에서 처리하고있긴 하지만 명시성을위해
+        }
+
+        return result;
+    }
+
+    int peek() {
+        if (this->head == nullptr) {
+            return 0;
+        }
+        return this->head->data;
+    }
+
+    bool isEmpty() { return this->head != nullptr ? 0 : 1; }
+};
+
+int main() {
+    Queue q;
+    q.add(1);
+    q.add(2);
+    q.add(3);
+    q.add(4);
+
+    std::cout << q.remove() << std::endl;
+    std::cout << q.remove() << std::endl;
+    std::cout << q.remove() << std::endl;
+    std::cout << q.peek() << std::endl;
+    std::cout << std::boolalpha << q.isEmpty() << std::endl;
+    std::cout << q.remove() << std::endl;
+    std::cout << q.isEmpty() << std::endl;
+
+    q.add(1);
+    std::cout << q.remove() << std::endl;
+
+    return 0;
+}
