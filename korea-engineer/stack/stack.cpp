4856fad2428c9a37f5c1c9446372f0d7011cd15e
luckyhyom <bs_khm@naver.com>
2025-11-25 18:41:41 +0900

    ds: Stack

--- korea-engineer/stack/stack.cpp
+++ korea-engineer/stack/stack.cpp
@@ -9,11 +9,16 @@ class Stack {
         Node(int n) : data(n) {};
     };
 
-    Node* head = nullptr;  // head는 진입점으로서 항상 필요하다.
+    // Node* head = nullptr;  // head는 진입점으로서 항상 필요하다. ❌
+    /**
+     * 제거하거나 추가하기 위해 필요한 맨 끝의 노드 하나만 변수에 저장한다.
+     * 링크드리스트와 달리, 중간 노드 조회가 필요 없어져서 시간 복잡도는 O(1)이다.
+     * Stack의 경우 노드를 링크드리스트와 반대의 순서로 추가해야한다.
+     */
     Node* top = nullptr;
 
    public:
-    Stack() { this->head = new Node(0); }
+    Stack() {}
 
     int pop() {
         if (this->top == nullptr) {
@@ -59,4 +64,7 @@ int main() {
     std::cout << std::boolalpha << s.isEmpty() << std::endl;
     std::cout << s.pop() << std::endl;
     std::cout << s.isEmpty() << std::endl;
+
+    s.push(1);
+    std::cout << s.pop() << std::endl;
 }
